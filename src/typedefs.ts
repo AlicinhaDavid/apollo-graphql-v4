1d0fac2f62512e54015fb65e74cd74d712d7ece7
AlicinhaDavid <alicedaviddossantos@gmail.com>
2023-08-24 13:20:25 -0300

    refactor: move type definition to a specific file

---
+++ src/typedefs.ts
@@ -0,0 +1,37 @@
+export const typeDefs = `#graphql
+  enum Color {
+    ORANGE
+    BLACK
+    RED
+    BLUE
+  }
+
+  type Picture {
+    source: String
+    product: Product
+  }
+
+  type Category {
+    name: String
+    products: [Product]
+  }
+
+  type Product {
+    id: String
+    name: String
+    description: String
+    price: String
+    categories: [Category]
+    picture: Picture
+    color: Color
+  }
+
+  type Query {
+    products: [Product]
+    productsByDescription(productDescription: String): [Product]
+    productsByCategory(categoryName: String): [Product]
+    productById(productId: String): Product,
+    categories: [Category]
+    pictures: [Picture]
+  }
+`;
