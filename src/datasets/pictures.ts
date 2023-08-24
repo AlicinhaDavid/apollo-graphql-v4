9d616e86751c6e3e0149c7e70f6d58bb45950005
AlicinhaDavid <alicedaviddossantos@gmail.com>
2023-08-24 13:46:19 -0300

    refactor: separate datasets in files

---
+++ src/datasets/pictures.ts
@@ -0,0 +1,30 @@
+export const pictures = [
+    {
+      source:
+        "https://unsplash.com/pt-br/fotografias/SdR2wW-v4PE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      product: {
+        name: "Orange pants",
+      },
+    },
+    {
+      source:
+        "https://unsplash.com/pt-br/fotografias/ylHcWlrMlzs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      product: {
+        name: "Black pants",
+      },
+    },
+    {
+      source:
+        "https://unsplash.com/pt-br/fotografias/3we24FcjVAk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      product: {
+        name: "Red pants",
+      },
+    },
+    {
+      source:
+        "https://unsplash.com/pt-br/fotografias/BN760VSO8yM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      product: {
+        name: "Blue T-Shirt",
+      },
+    },
+  ];
