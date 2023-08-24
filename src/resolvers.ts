3dd631c511950a284aacb46c7a758e5f1d79eaac
AlicinhaDavid <alicedaviddossantos@gmail.com>
2023-08-24 17:18:41 -0300

    refactor: separate resolvers and business logic

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,24 +2,46 @@ import { products } from "./datasets/products";
 import { categories } from "./datasets/categories";
 import { pictures } from "./datasets/pictures";
 
+const getProducts = () =>{
+  return products
+}
+
+const getProductsByDescription = (description: string) => {
+  return products.filter((product) => {
+    return product.description.includes(description);
+  });
+};
+
+const getProductsByCategory = (categoryName: string) => {
+  return products.filter((product) => {
+    return product.categories.find((category) => {
+      return category.name === categoryName;
+    });
+  });
+};
+
+const getProductById = (id: string) => {
+  products.find((product) => {
+    return product.id === id;
+  });
+};
+
+const getCategories = () =>{
+  return categories
+}
+
+const getPictures = () =>{
+  return pictures
+}
+
 export const resolvers = {
   Query: {
-    products: () => products,
+    products: getProducts(),
     productsByDescription: (root, args, context, info) =>
-      products.filter((product) => {
-        return product.description.includes(args.productDescription);
-      }),
-    productsByCategory: (root, args, context, info) =>
-      products.filter((product) => {
-        return product.categories.find((category) => {
-          return category.name === args.categoryName;
-        });
-      }),
-    productById: (root, args, context, info) =>
-      products.find((product) => {
-        return product.id === args.productId;
-      }),
-    categories: () => categories,
-    pictures: () => pictures,
+      getProductsByDescription(args.productDescription),
+    productsByCategory: (root, args, context, info) => getProductsByCategory(args.categoryName),
+    productById: (root, args, context, info) => getProductById(args.productId),
+    categories: getCategories(),
+    pictures: getPictures(),
   },
 };
